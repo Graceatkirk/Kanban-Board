0734d69eae4e26d6bff4be2fce4c39f44777acff
Graceatkirk <grace@kirk.org>
2025-01-26 19:28:46 -0600

    weeeee

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Updated CORS configuration
-app.use(cors({
-  origin: 'http://localhost:3000', // React's default port
-  credentials: true
-}));
 
 app.use(express.static('../client/dist'));
 app.use(express.json());
