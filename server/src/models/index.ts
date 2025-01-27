2e06aed5d094d9d7371bd9fad27269311d5a0b74
Graceatkirk <grace@kirk.org>
2025-01-26 19:08:34 -0600

    only check for local vars locally

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,25 +5,27 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD) {
-  throw new Error('Database configuration environment variables are missing');
-}
+
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize({
-      database: process.env.DB_NAME,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-      logging: console.log
-    });
-
+  : (() => {
+      if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD) {
+        throw new Error('Missing required database environment variables');
+      }
+      return new Sequelize({
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        port: 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+        logging: console.log
+      });
+    })();
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
